bd5e0ae22002d789e69c7ad8dc92682448cdb8bb
Deividi_Lucas <113848434+Deividi-Lucas@users.noreply.github.com>
2022-12-25 20:57:02 -0400

    Projeto finalizado
    Adicionado as funções

--- exer5/exer5.js
+++ exer5/exer5.js
@@ -2,12 +2,22 @@ let num = window.document.querySelector('input#numero')
 let msg = window.document.querySelector('div#msg')
 let list = []
 let res = window.document.querySelector('div#res')
+var c = 0
+let soma = 0
+function s() {
+    while (c < list.length) {
+        soma = soma + Number(list[c])
+        c++
+    }
+}
+s()
 
 function adicionar() {
     if (Number(num.value) >= 1 && Number(num.value) <=100){
         if (list.indexOf(num.value) == -1) {
             list.unshift(num.value)
             msg.innerHTML = `o valor ${num.value} foi adicionado`
+            list.sort()
         }else{
             alert(`o valor ${num.value} já está na lista!`)
         }
@@ -23,14 +33,17 @@ function adicionar() {
 
 function finalizar(){
     if (list.length ==0){
-        console.log(list.length);
+        alert('Favor adicionar valores para que seja finalizado o programa.')
     }else {
+        s()
+        res.innerHTML = ''
         res.innerHTML += `<br>A lista tem ${list.length} elementos` 
-        res.innerHTML += `<br>São eles ${list}`
-        res.innerHTML += `<br>A lista tem o valor {} como o maior `
-        res.innerHTML += `<br>A lista tem o valor {} como o menor`
-        res.innerHTML += `<br>A lista tem a soma dos valores {} `
-        res.innerHTML += `<br>A lista tem o valor médio {}`
+        res.innerHTML += `<br>São eles ${list.sort()}`
+        res.innerHTML += `<br>A lista tem o valor ${Math.max(...list)} como o maior `
+        res.innerHTML += `<br>A lista tem o valor ${Math.min(...list)} como o menor`
+        res.innerHTML += `<br>A lista tem a soma dos valores ${soma} `
+        let media = soma / list.length
+        res.innerHTML += `<br>A lista tem o valor médio ${media}`
 
     }
     
