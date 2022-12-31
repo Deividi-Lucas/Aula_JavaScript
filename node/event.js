0fe5ebfcef9eb684f8bb2ecec790e837e636bd25
Deividi_Lucas <113848434+Deividi-Lucas@users.noreply.github.com>
2022-12-30 20:04:49 -0400

    event

--- node/event.js
+++ node/event.js
@@ -1,3 +1,20 @@
+// dia 30 de dez  
+
+let btn = window.document.getElementById("botao")
+let color = document.getElementById('cor')
+
+function teste (){
+    alert('funcionou')
+}
+
+function cor (){
+    document.body.style.background = "red"
+}
+
+btn.addEventListener("click", teste)    // a chamada vem antes depois vem a função sem conchete
+color.addEventListener("click", cor)
+
+//
 var a = document.getElementById('area')
         a.addEventListener('click', clicar)
         a.addEventListener('mousedown', clicando)
